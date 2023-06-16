8e854df3e6415cb10577aeb7bf7d63f2425fda5d
Omar Jano <omar.jano05@gmail.com>
2023-06-16 14:59:57 +0300

    added helpers folder and varialbes

---
+++ helpers/utils.ts
@@ -0,0 +1,71 @@
+export const sleep = (ms: number) => {
+  new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+// - Helpers
+
+/**
+ * Trims extra whitespace and lowercases the text
+ * @param text The text.
+ * @param lowerCase Option to lowerCase the text. Defaults to true.
+ * @returns Cleared up text.
+ * @example cleanUp("   Text HERE ")
+ */
+
+export function cleanUp(text: string, lowerCase = true) {
+  const res = text.trim().replace(/\s+/g, " ");
+  return lowerCase ? res.toLowerCase() : res;
+}
+
+/**
+ * Trims extra whitespace, split it and uppercase first letter then it joins to create a string.
+ *
+ * @param text The text.
+ * @returns Capitalized text.
+ */
+export function capitalizeLetters(text: string) {
+  return text
+    .toLowerCase()
+    .trim()
+    .split(" ")
+    .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+    .join(" ");
+}
+
+/**
+ * Removes all whitespace.
+ *
+ * @param text The text.
+ * @returns Text without any whitespace.
+ */
+export function removeWhiteSpaces(text: string) {
+  return text.replace(/\s/g, "");
+}
+
+/**
+ * Generates random number.
+ * @param length The length of the generated number.
+ * @returns Random set of numbers.
+ */
+export function digitGen(length: number) {
+  let digit = "";
+  for (let index = 0; index < length; index++) {
+    digit += Math.round(Math.random() * 9);
+  }
+  return digit;
+}
+/**
+ * It validates the string or the array. It checks if the length is bigger than zero. Since split returns [''], we need to check multiple times for the Array
+ * @param text The text.
+ * @returns
+ */
+export function lengthValidator(text: string | string[]) {
+  if (Array.isArray(text))
+    return text.length > 0 && text[0].length > 0 ? true : false;
+  if (text.trim().length <= 0) return false;
+  return true;
+}
+
+export function numberValidator(value: string) {
+  return !isNaN(parseInt(value)) && lengthValidator(value);
+}
